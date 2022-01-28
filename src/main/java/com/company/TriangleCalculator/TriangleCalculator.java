e86ad4a26aadada16d967abf606730dd29b43a35
Jose Salgado Jr <jjsalga1@uci.edu>
2022-01-27 16:33:08 -0800

    Completed module 2 challenge.

--- src/main/java/com/company/TriangleCalculator/TriangleCalculator.java
+++ src/main/java/com/company/TriangleCalculator/TriangleCalculator.java
@@ -6,10 +6,20 @@ public class TriangleCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter length of adjacent side of right triangle:");
-        double adj = Integer.parseInt(scan.nextLine());
+        double adj = Double.parseDouble(scan.nextLine());
         System.out.println("Please enter length of opposite side of right triangle:");
-        double opp = Integer.parseInt(scan.nextLine());
+        double opp = Double.parseDouble(scan.nextLine());
+
+        // Calculate hypotenuse
         double hyp = Math.sqrt((adj * adj) + (opp * opp));
 
+        // SOH-CAH-TOA
+        double sine = opp / hyp;
+        double cosine = adj / hyp;
+        double tangent = opp / adj;
+
+        System.out.println("Sine: " + sine);
+        System.out.println("Cosine: " + cosine);
+        System.out.println("Tangent: " + tangent);
     }
 }
